8ca404f939a51ba7790890b439e3660d2f649d87
Mike Setzer <michaelsetzerucf@gmail.com>
2017-10-04 13:46:39 -0400

    Revert "Merge branch 'master' into Dev"
    This reverts commit 0c43b8f1287d6bf2889a6eecb57bcbc77fb5c14f, reversing
    changes made to 61e6e43aa3b2bbe3e610c9050fc43abb5aee747c.

--- functions.php
+++ functions.php
@@ -1,135 +1,151 @@
 <?php
+/**
+ * UCF Business Services
+ *
+ * This file adds functions to the UCF Business Services Genesis Theme.
+ *
+ * @package UCF Business Services Genesis 1.0
+ * @author  Mike Setzer
+ * @license GPL-2.0+
+ * @link    https://github.com/UCF/BS-WPTheme-Template
+ */
 
-error_reporting(E_ALL);
+// Start the engine.
+include_once( get_template_directory() . '/lib/init.php' );
 
-show_admin_bar(false);
+// Setup Theme.
+include_once( get_stylesheet_directory() . '/lib/theme-defaults.php' );
 
-if(!defined('THEME_URL'))
-	define('THEME_URL', get_bloginfo('template_directory'));
+// Set Localization (do not remove).
+add_action( 'after_setup_theme', 'genesis_sample_localization_setup' );
+function genesis_sample_localization_setup(){
+	load_child_theme_textdomain( 'genesis-sample', get_stylesheet_directory() . '/languages' );
+}
 
-//	fix db after server move
-//require_once( TEMPLATEPATH.'/library/includes/mysql-replace.php' );
-//MySQL_Replace::replace('old', 'new');
+// Add the helper functions.
+include_once( get_stylesheet_directory() . '/lib/helper-functions.php' );
 
-//	dependicies
-require_once( TEMPLATEPATH.'/library/includes/wp-header-remove.php' );
+// Add Image upload and Color select to WordPress Theme Customizer.
+require_once( get_stylesheet_directory() . '/lib/customize.php' );
 
-//	menus
-register_nav_menus( array(
-	'main-nav' => 'Main Navigation'
+// Include Customizer CSS.
+include_once( get_stylesheet_directory() . '/lib/output.php' );
+
+// Add WooCommerce support.
+include_once( get_stylesheet_directory() . '/lib/woocommerce/woocommerce-setup.php' );
+
+// Add the required WooCommerce styles and Customizer CSS.
+include_once( get_stylesheet_directory() . '/lib/woocommerce/woocommerce-output.php' );
+
+// Add the Genesis Connect WooCommerce notice.
+include_once( get_stylesheet_directory() . '/lib/woocommerce/woocommerce-notice.php' );
+
+// Child theme (do not remove).
+define( 'CHILD_THEME_NAME', 'Genesis Sample' );
+define( 'CHILD_THEME_URL', 'http://www.studiopress.com/' );
+define( 'CHILD_THEME_VERSION', '2.3.0' );
+
+// Enqueue Scripts and Styles.
+add_action( 'wp_enqueue_scripts', 'genesis_sample_enqueue_scripts_styles' );
+function genesis_sample_enqueue_scripts_styles() {
+
+	wp_enqueue_style( 'genesis-sample-fonts', '//fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700', array(), CHILD_THEME_VERSION );
+	wp_enqueue_style( 'dashicons' );
+
+	$suffix = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
+	wp_enqueue_script( 'genesis-sample-responsive-menu', get_stylesheet_directory_uri() . "/js/responsive-menus{$suffix}.js", array( 'jquery' ), CHILD_THEME_VERSION, true );
+	wp_localize_script(
+		'genesis-sample-responsive-menu',
+		'genesis_responsive_menu',
+		genesis_sample_responsive_menu_settings()
+	);
+
+}
+
+// Define our responsive menu settings.
+function genesis_sample_responsive_menu_settings() {
+
+	$settings = array(
+		'mainMenu'          => __( 'Menu', 'genesis-sample' ),
+		'menuIconClass'     => 'dashicons-before dashicons-menu',
+		'subMenu'           => __( 'Submenu', 'genesis-sample' ),
+		'subMenuIconsClass' => 'dashicons-before dashicons-arrow-down-alt2',
+		'menuClasses'       => array(
+			'combine' => array(
+				'.nav-primary',
+				'.nav-header',
+			),
+			'others'  => array(),
+		),
+	);
+
+	return $settings;
+
+}
+
+// Add HTML5 markup structure.
+add_theme_support( 'html5', array( 'caption', 'comment-form', 'comment-list', 'gallery', 'search-form' ) );
+
+// Add Accessibility support.
+add_theme_support( 'genesis-accessibility', array( '404-page', 'drop-down-menu', 'headings', 'rems', 'search-form', 'skip-links' ) );
+
+// Add viewport meta tag for mobile browsers.
+add_theme_support( 'genesis-responsive-viewport' );
+
+// Add support for custom header.
+add_theme_support( 'custom-header', array(
+	'width'           => 600,
+	'height'          => 160,
+	'header-selector' => '.site-title a',
+	'header-text'     => false,
+	'flex-height'     => true,
 ) );
 
-//	post thumbnails
-add_theme_support( 'post-thumbnails' );
+// Add support for custom background.
+add_theme_support( 'custom-background' );
 
-//GForms Dropbox SSL Filter
-add_filter( 'gform_dropbox_ssl_compatibility', '__return_true' );
+// Add support for after entry widget.
+add_theme_support( 'genesis-after-entry-widget-area' );
 
-/* Google Analytics Settings Code */
-$google_analytics_setting = new google_analytics_setting();
-class google_analytics_setting {
+// Add support for 3-column footer widgets.
+add_theme_support( 'genesis-footer-widgets', 3 );
 
-    function google_analytics_setting() {
-        add_filter( 'admin_init' , array( &$this , 'register_fields' ) );
-    }
+// Add Image Sizes.
+add_image_size( 'featured-image', 720, 400, TRUE );
 
-    function register_fields() {
-        register_setting( 'general', 'google_analytics_code', '' );
-        add_settings_field( 'fav_color', '<label for="google_analytics_code">' . __( 'Google Analytics Code' , 'google_analytics_code' ).'</label>' , array( &$this, 'fields_html' ) , 'general' );
-    }
+// Rename primary and secondary navigation menus.
+add_theme_support( 'genesis-menus', array( 'primary' => __( 'After Header Menu', 'genesis-sample' ), 'secondary' => __( 'Footer Menu', 'genesis-sample' ) ) );
 
-    function fields_html() {
-        $value = get_option( 'google_analytics_code', '' );
-        echo '<textarea rows="7" id="google_analytics_code" name="google_analytics_code" style="width: 100%;">' . $value . '</textarea>';
-    }
+// Reposition the secondary navigation menu.
+remove_action( 'genesis_after_header', 'genesis_do_subnav' );
+add_action( 'genesis_footer', 'genesis_do_subnav', 5 );
 
-}
+// Reduce the secondary navigation menu to one level depth.
+add_filter( 'wp_nav_menu_args', 'genesis_sample_secondary_menu_args' );
+function genesis_sample_secondary_menu_args( $args ) {
 
-/* Print Within WP Head */
-add_action( 'wp_head', 'google_head' );
-function google_head() {
-	/* Google Analytics */ 
-	if ( get_option( 'google_analytics_code' ) != FALSE ) { 
-		echo get_option( 'google_analytics_code', '' );
-	}
-}
-
-#	UCF Scripts
-########################################################
-
-/**
- * Is login or register page
- * @author umbrovskis
- * @authorlink http://umbrovskis.com 
- */
-function smc_is_login_page() {
-    return in_array( $GLOBALS['pagenow'], array( 'wp-login.php', 'wp-register.php' ) );
-}
-
-#	Scripts
-########################################################
-add_action( 'wp_print_scripts', 'spry_print_scripts' );
-
-function spry_print_scripts() {
-	
-	if( is_admin() )
-		return false;
-	
-	wp_enqueue_script( 'jquery' );
-	wp_register_script( 'lib', THEME_URL.'/library/js/lib.js', array( 'jquery' ), '', true );
-	wp_enqueue_script( 'lib' );
-	wp_register_script( 'infobubble', THEME_URL.'/library/js/infobubble.js', array( 'jquery' ), '' );
-
-	/* Localize */
-	wp_localize_script( 'lib', 'SPRY', array(
-		'ajaxurl' => admin_url( 'admin-ajax.php' ),
-		'siteurl' => get_option('siteurl'),
-		'themeUrl' => THEME_URL
-	) );
-
-	if ( !smc_is_login_page() ) {
-		wp_enqueue_script( 'ucfhb', '//universityheader.ucf.edu/bar/js/university-header.js', array( 'jquery' ) );
+	if ( 'secondary' != $args['theme_location'] ) {
+		return $args;
 	}
 
-	wp_enqueue_script( 'infobubble' );
+	$args['depth'] = 1;
 
-	if ( get_page_template( 'template-contact.php' ) ) {
-		wp_enqueue_script( 'google-maps', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyB6IftJIbhuHExsKKkPqARDNmkp3kZKhS4', array( 'jquery' ) );
-		wp_enqueue_script( 'contact-js', THEME_URL . '/library/js/contact.js', array( 'jquery' ) );
-	}
+	return $args;
 
 }
 
-#	General Functions
-########################################################
-
-//custom length excerpt
-function my_excerpt($limit) {
-  $excerpt = explode(' ', get_the_excerpt(), $limit);
-  if (count($excerpt)>=$limit) {
-	array_pop($excerpt);
-	$excerpt = implode(" ",$excerpt).'...';
-  } else {
-	$excerpt = implode(" ",$excerpt);
-  } 
-  $excerpt = preg_replace('`\[[^\]]*\]`','',$excerpt);
-  return $excerpt;
+// Modify size of the Gravatar in the author box.
+add_filter( 'genesis_author_box_gravatar_size', 'genesis_sample_author_box_gravatar' );
+function genesis_sample_author_box_gravatar( $size ) {
+	return 90;
 }
 
-// Add Shortcode
-function links_shortcode( $atts ) {
+// Modify size of the Gravatar in the entry comments.
+add_filter( 'genesis_comment_list_args', 'genesis_sample_comments_gravatar' );
+function genesis_sample_comments_gravatar( $args ) {
 
-	// Attributes
-	extract( shortcode_atts(
-		array(
-			'a' => '#',
-			'b' => '',
-			'c' => '#',
-			'd' => ''
-		), $atts )
-	);
-	
-	return '<div class="cm-job-links"><a class="first-link" href="'.$a.'">'.$b.'<span>VIEW JOB OPENINGS IN</span></a><a class="second-link" href="'.$c.'">'.$d.'<span>VIEW JOB OPENINGS IN</span></a></div>';
-	
+	$args['avatar_size'] = 60;
+
+	return $args;
+
 }
-add_shortcode( 'job_links', 'links_shortcode' );
