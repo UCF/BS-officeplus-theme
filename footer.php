322e32d3c3210f09b17613cb3628c86da137eb40
Mike Setzer <michaelsetzerucf@gmail.com>
2018-07-10 13:07:32 -0400

    Business Services Theme moved

--- footer.php
+++ footer.php
@@ -2,41 +2,41 @@
 
 <footer id="footer">
 
-    <div class="container"> 
+	<div class="container">
 
-        <div class="footer-copy pull-left"> 
+		<div class="copy-container pull-left">
 
-            <a href="<?php echo get_option('siteurl'); ?>" class="logo"></a>
-            <?php //site logo here?> 
+			<a href="<?php echo get_option('siteurl'); ?>" class="logo"></a>
+			<?php /* spry dev id <a href="<?php echo get_permalink( 2 ); ?>" class="logo"></a> */ ?>
 
-            <div class="copy-text">
-                <p>&copy; <?php echo date('Y'); ?> <!-- SITE NAME AND TAGLINE HERE --> </p> 
-                <!--<p>Phone: <a href="tel:">(407) </a> | Fax: <a href="tel:">(407)</a> | 12479 Research Pkwy Ste 600, Orlando, FL 32826</p>--> 
-            </div>
+			<div class="copy-text">
+				<p>&copy; <?php echo date('Y'); ?> UCF Business Services, a unit of the Division of Administration and Finance</p>
+				<p>Phone: <a href="tel:4078232624">(407) 823-2624</a> | Fax: <a href="tel:4078820175">(407) 882-0175</a> | 12479 Research Pkwy Ste 600, Orlando, FL 32826</p>
+			</div>
+			
+		</div>
 
-        </div> 
+		<div class="link-container pull-right">
 
-        <div class="footer-links pull-right"> 
+			<div class="pull-right">
+			
+				<div class="menu-wrap pull-left">
+					<?php wp_nav_menu( array( 'theme_location' => 'main-nav', 'container' => '' ) ); ?>
+				</div>
 
-            <div class="pull right"> 
+				<a href="#" class="pull-right up-arrow"></a>
 
-                <div class="menu-wrap pull-left"> 
-                    <?php wp_nav_menu( array( 'theme_location' => 'main-nav', 'container' => '') ); ?> 
-                </div>
+				<div class="clear"></div>
 
-                <a href="#" class="pull-right up-arrow"></a>
+			</div>
 
-                <div class="clear"></div>
-            
-            </div>
+			<div class="clear"></div>
+			
+		</div>
 
-            <div class="clear"></div>
+		<div class="clear"></div>
 
-        </div>
-
-        <div class="clear"></div>
-
-    </div>
+	</div>
 
 </footer>
 
--- header.php
+++ header.php
@@ -6,32 +6,39 @@
 <!--[if gt IE 8]><!--> <html <?php language_attributes(); ?> class="no-js<?php if ( is_404() ) { ?> error404<?php } ?>"><!--<![endif]-->
 
 <head>
-    <meta charset="utf-8">
-
-    <title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
-    
-    <?php //mobile meta ?>
-    <meta name="HandheldFriendly" content="True">
+	<meta charset="utf-8">
+	
+	<title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
+	
+	<?php //mobile meta (hooray!) ?>
+	<meta name="HandheldFriendly" content="True">
 	<meta name="MobileOptimized" content="320">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
 	
 	<?php //hide iOS browser bar ?>
 	<meta name="apple-mobile-web-app-capable" content="yes" />
 
-    <?php //Stylesheet imports ?> 
-    <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/lib/scss/styles.css" type="text/css" media="screen" />
-    <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/style.css" type="text/css" media="screen" />
-    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" /> 
-
-    <?php //Font imports ?>
-    <!-- <script src=" "></script>-->
-
-    <?php wp_head(); ?>
+	<!--[if IE]>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<![endif]-->
+	<!--[if lt IE 9]>
+		<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+	<![endif]-->
+	
+	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/library/css/style.css" type="text/css" media="screen" />
+	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+	<?php /* <link rel="shortcut icon" href="<?php bloginfo('stylesheet_directory'); ?>/library/images/favicon.png" /> */ ?>
+	
+	<?php /* TypeKit */ ?>
+	<script src="https://use.typekit.net/zpc6gqm.js"></script>
+	<script>try{Typekit.load({ async: true });}catch(e){}</script>
+	
+	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 
 <div id="wrap">
+	
+	<?php get_template_part( 'template-parts/menu' ); ?>
 
-    <?php get_template_part( 'lib/menu' ); ?>
-    
